c702585767b5e6f3003b2bb548f2814fa10bbc01
Frank Fang <frankfang1990@gmail.com>
2014-05-18 21:51:36 +0800

    show images!

--- chrome-app/app.js
+++ chrome-app/app.js
@@ -2,7 +2,9 @@ var $ = require('jquery')
 var channel = require('./vendors/channel')
 
 var source = 'http://www.ruanyifeng.com/blog/atom.xml'
-channel.get(source, function (error, feed) {
+
+
+function onGetFeed(error, feed) {
     var item = channel.renderBlog(feed)
     console.log("feed:")
     console.log(feed)
@@ -15,12 +17,27 @@ channel.get(source, function (error, feed) {
             entries.forEach(function (entry) {
                 var li = document.createElement('li')
                 li.appendChild(entry)
+
+                var images = li.querySelectorAll('img')
+                images = Array.prototype.slice.call(images, 0)
+                images.forEach(function (image) {
+                    var xhr = new XMLHttpRequest();
+                    xhr.open('GET', image.src, true);
+                    xhr.responseType = 'blob';
+                    xhr.onload = function (e) {
+                        image.src = window.URL.createObjectURL(this.response);
+                    };
+
+                    xhr.send();
+                })
+
                 ol.appendChild(li)
             })
             document.body.appendChild(frag)
 
+
         })
-})
+}
 
 
 
